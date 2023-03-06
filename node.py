9753287c6248151a6002076d9f0e17cc1c61b19b
guneyizol <guneyizol@gmail.com>
2023-03-06 15:06:39 +0300

    fix message flow bugs

--- node.py
+++ node.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 import sys
 import signal
+import aioconsole
 
 
 def keyboardInterruptHandler(signal, frame):
@@ -15,32 +16,40 @@ ip_dict = {}
 myip = '192.168.1.0'
 myname = 'name'
 
+
 async def send_hello_to_ip(ip, myip):
-    proc = await asyncio.create_subprocess_exec(
-        'nc', str(ip), '12345',
-        stdout=asyncio.subprocess.PIPE,
-        stdin=asyncio.subprocess.PIPE
-    )
+    if ip not in ip_dict.values():
+        proc = await asyncio.create_subprocess_exec(
+            'nc', ip, '12345',
+            stdout=asyncio.subprocess.PIPE,
+            stdin=asyncio.subprocess.PIPE
+        )
 
-    proc.stdin.write((json.dumps({
-        'type': 'hello',
-        'myname': 'guney',
-        'myip': myip
-    }
-    ) + '\n').encode()
-    )
+        proc.stdin.write((json.dumps({
+            'type': 'hello',
+            'myname': 'guney',
+            'myip': myip
+        }
+        ) + '\n').encode()
+        )
 
-    data = await proc.stdout.readline()
-    line = data.decode().rstrip()
-    if line:
-        hello_message = json.loads(line)
-        if hello_message.get('type') == 'aleykumselam':
-            try:
-                ip_dict[
-                    hello_message['myname']
-                ] = hello_message['myip']
-            except KeyError:
-                pass
+        try:
+            data = await asyncio.wait_for(proc.stdout.readline(), timeout=10)
+            line = data.decode().rstrip()
+            if line:
+                hello_message = json.loads(line)
+                if hello_message.get('type') == 'aleykumselam':
+                    try:
+                        ip_dict[
+                            hello_message['myname']
+                        ] = hello_message['myip']
+                    except KeyError:  # ignore invalid responses
+                        pass
+                    finally:
+                        proc.terminate()  # if there is a connection, close it
+        except asyncio.exceptions.TimeoutError:
+            proc.terminate()  # if there is a connection, close it
+        
 
 
 async def listen(myip):
@@ -53,21 +62,22 @@ async def listen(myip):
     while True:
         data = await proc.stdout.readline()
         line = data.decode().rstrip()
-        hello_message = json.loads(line)
-        print(hello_message)
-        if hello_message.get('type') == 'hello':
-            try:
-                ip_dict[hello_message['myname']] = hello_message['myip']
-                proc.stdin.write(
-                    (json.dumps({
-                        'type': 'aleykumselam',
-                        'myname': 'guney',
-                        'myip': myip
-                    }
-                    ) + '\n').encode()
-                )
-            except KeyError:
-                pass
+        if line:
+            hello_message = json.loads(line)
+            print(hello_message)
+            if hello_message.get('type') == 'hello':
+                try:
+                    ip_dict[hello_message['myname']] = hello_message['myip']
+                    proc.stdin.write(
+                        (json.dumps({
+                            'type': 'aleykumselam',
+                            'myname': 'guney',
+                            'myip': myip
+                        }
+                        ) + '\n').encode()
+                    )
+                except KeyError:
+                    pass
 
 
 async def send_hello(myip):
@@ -85,35 +95,35 @@ async def send_hello(myip):
 
 
 async def send_message():
+    ip = await aioconsole.ainput('enter recipient IP: ')
+    message = await aioconsole.ainput('enter your message (end it with a newline): ')
+
+    proc = await asyncio.create_subprocess_exec(
+        'nc', ip, '12345',
+        stdout=asyncio.subprocess.PIPE,
+        stdin=asyncio.subprocess.PIPE
+    )
+
+    proc.stdin.write((json.dumps({
+        'type': 'message',
+        'content': message,
+        'myip': myip
+    }
+    ) + '\n').encode()
+    )
+
+
+async def control():
+    key = await aioconsole.ainput('To send a message, press M\n'
+                                      'To see the available recipient IPs, press A\n'
+                                      'To exit, press E\n')
     while True:
-        print('To send a message, press M')
-        print('To see the available recipient IPs, press A')
-        print('To exit, press E')
-        
-        key = input().lower()
+        key = key.lower()
         if key == 'm':
             if not ip_dict:
                 print('There are no available recipients. Try later.')
             else:
-                print('enter recipient IP: ', end='')
-                ip = input()
-
-                print('enter your message (end it with a newline): ')
-                message = input()
-
-                proc = await asyncio.create_subprocess_exec(
-                    'nc', str(ip), '12345',
-                    stdout=asyncio.subprocess.PIPE,
-                    stdin=asyncio.subprocess.PIPE
-                )
-
-                proc.stdin.write((json.dumps({
-                    'type': 'hello',
-                    'content': message,
-                    'myip': myip
-                }
-                ) + '\n').encode()
-                )
+                await asyncio.create_task(send_message())
         elif key == 'a':
             if not ip_dict:
                 print('There are no available recipients.')
@@ -121,19 +131,19 @@ async def send_message():
                 print(f'{name}: {ip}')
         elif key == 'e':
             sys.exit(0)
+        
+        key = await aioconsole.ainput()
 
 
 async def main():
-    print('enter your ip: ', end='')
-
     global myip
-    myip = input()
+    myip = await aioconsole.ainput('Enter your ip: ')
 
     listen_task = asyncio.create_task(listen(myip))
     hello_task = asyncio.create_task(send_hello(myip))
-    message_task = asyncio.create_task(send_message())
+    control_task = asyncio.create_task(control())
 
-    await asyncio.gather(listen_task, hello_task, message_task)
+    await asyncio.gather(listen_task, hello_task, control_task)
 
 
 asyncio.get_event_loop().run_until_complete(main())
